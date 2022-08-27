6abf17040581c8f164c40a61bf72e3ccf85cae7a
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2022-08-27 19:21:06 +0200

    update lib/primes

--- lib/primes.R
+++ lib/primes.R
@@ -1,6 +1,4 @@
 
-# Erathostenes ------------------------------------------------------------
-
 #' Sieve of Erathostenes
 #'
 #' @param to upper bound of primes to return
@@ -11,48 +9,48 @@
 #' @examples primes_to_11 <- sieve_ov_Erathostenes(11)
 sieve_ov_Erathostenes <- function(to){
   
-  # exceptions
-  if (to == 2){
-    return(2)
-  } else if (to == 3){
-    return(3)
-  }
+  # Exceptions
+  assertthat::assert_that(to >= 2,
+                          msg = "There are no primes below 2!!!")
   
-  # vars
-  upperBound = round(sqrt(to))
+  # Edge Cases
+  if (to %in% c(2,3)){
+    return(to)
+  } 
   
-  # generate vec of boolean of length "to"
-  vecNat = rep(T, to)
+  # We only need to check up to sqrt(to) as everything bigger than that
+  upper_bound = round(sqrt(to))
+  
+  # Generate vec of boolean of length "to"
+  vec_nat = rep(TRUE, to)
   
   # set 1 to false, edge case
-  vecNat[1] <- F
+  vec_nat[1] <- FALSE
   
   # sieve of erathostenes
   # start with 2
-  lastPrime = 2
+  last_prime = 2
   
-  for (i in lastPrime:upperBound){
-    # skip indeces that are already F
-    if (vecNat[i] == F){
-      lastPrime = lastPrime + 1
+  for (i in last_prime:upper_bound){
+    # skip indeces that are already FALSE
+    if (vec_nat[i] == FALSE){
+      last_prime = last_prime + 1
       next
     }
     
     # find all multiples of primes as these must be composite
-    multiples <- seq(lastPrime*2, to, lastPrime)
-    vecNat[multiples] <- F
+    multiples <- seq(last_prime*2, to, last_prime)
+    vec_nat[multiples] <- FALSE
     
-    # redefine new lastPrime
-    lastPrime = lastPrime + 1
+    # redefine new last_prime
+    last_prime = last_prime + 1
   }
   
   # return primes, i.e. those that are still TRUE
-  return(which(vecNat))
+  return(which(vec_nat))
 }
 
-# Cyclic Primes -----------------------------------------------------------
-
-#' Get cyclic primes - E.g. 113, 131, and 311, i.e. cyclic permutatios that
+#' Get circular primes - E.g. 113, 131, and 311, i.e. circular permutatios that
 #' are also primes
 #'
 #' @param primes 
@@ -61,16 +59,16 @@ sieve_ov_Erathostenes <- function(to){
 #' @export
 #'
 #' @examples primes_to_11 <- sieve_ov_Erathostenes(11)
-#' get_cyclic_primes(primes_to_11)
-get_cyclic_primes <- function(primes){
+#' get_circular_primes(primes_to_11)
+get_circular_primes <- function(primes){
   
   # Cache vector for primes - reduces the number of iteration if primes are already in
-  # this vectior. E.g. a cyclic prime with 3 digits will appear 3 times and should only
+  # this vectior. E.g. a circular prime with 3 digits will appear 3 times and should only
   # be evaluated once.
-  rotVecCache <- c()
+  rot_vec_cache <- c()
   
   # empty vector for output$
-  cyclicPrimes <- c()
+  circular_primes <- c()
   
   cnt = 0
   
@@ -78,31 +76,31 @@ get_cyclic_primes <- function(primes){
   primes = primes[!grepl("0|[0-9]+2|4|6|8", primes)]
   
   # go through all rotations in all primes of the slice primesRot
-  for(potCyclicPrimes in primes){
+  for(pot_circular_primes in primes){
     
     # transform prime to character vector to evaluate rotation
-    rot <- as.character(potCyclicPrimes)
-    rotVec <- unlist(strsplit(rot,""))
+    rot <- as.character(pot_circular_primes)
+    rot_vec <- unlist(strsplit(rot,""))
     
     # rotation
     check = 0
     
-    rotation = length(rotVec)
+    rotation = length(rot_vec)
     
     # evaluate if all rotations are also primes 
     for (i in 1:rotation){
       
-      lastIdx <- length(rotVec)
+      last_idx <- length(rot_vec)
       
       # puts the last index at the first position and pushes the OG-vec to position 2
-      rotVec <- rotVec[c(lastIdx,1:lastIdx-1)]
+      rot_vec <- rot_vec[c(last_idx,1:last_idx-1)]
       
-      # if rotVec not in cache check whether rotations are primes
-      rotVecNum <- as.numeric(paste(rotVec, collapse = "",sep = ""))
+      # if rot_vec not in cache check whether rotations are primes
+      rot_vec_num <- as.numeric(paste(rot_vec, collapse = "",sep = ""))
       cnt = cnt + 1
       
       # check if rotation is prime
-      if(rotVecNum %in% primes){
+      if(rot_vec_num %in% primes){
         # if rotation is in primes, add +1 to check counter  
         check = check + 1
       } else {
@@ -111,13 +109,11 @@ get_cyclic_primes <- function(primes){
     }
     # if the number of checks is equal to the number of rotation all rotation are primes
     if (check == rotation){
-      cyclicPrimes <- append(cyclicPrimes, potCyclicPrimes)
+      circular_primes <- append(circular_primes, pot_circular_primes)
     }
   }
-  return(cyclicPrimes)
+  return(circular_primes)
 }
-
-# Coin change algo --------------------------------------------------------
 
 #' Title
 #'
@@ -128,6 +124,8 @@ get_cyclic_primes <- function(primes){
 #' @export
 #'
 #' @examples
+
+# TODO: Does not yet work
 coin_change_algo <- function(coins, N){
   
   # create vars
@@ -151,4 +149,5 @@ coin_change_algo <- function(coins, N){
 
 # TODO: shouldnt this be 3? 1,1,1; 1,1,2; 2,2
 coin_change_algo(c(1,2),4)
+
 
